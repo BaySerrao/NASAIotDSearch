845bf7106d031bce34002bf35a10fb310ede3db0
Bay Serrao <serr0063@algonquinlive.ca>
2022-05-29 17:59:31 -0400

    Lab 2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_grid);
+        Button btn = (Button)findViewById(R.id.button1);
+        TextView t1 = (TextView)findViewById(R.id.text1);
+        EditText e1 = (EditText)findViewById(R.id.etext1);
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Editable new1 = e1.getText();
+                t1.setText(new1);
+                Toast.makeText(MainActivity.this, getString(R.string.toast), Toast.LENGTH_SHORT).show();
+            }
+        });
+        CheckBox c1 = (CheckBox)findViewById(R.id.cb1);
+        c1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (c1.isChecked()){
+                    Snackbar.make(c1, getString(R.string.csnack), Snackbar.LENGTH_LONG)
+                            .setAction("Undo", click -> c1.setChecked(false))
+                            .show(); }
+                else{
+                    Snackbar.make(c1, getString(R.string.csnack2), Snackbar.LENGTH_LONG)
+                            .setAction("Undo", click -> c1.setChecked(true))
+                            .show();
+                }
+
+            }
+        });
+
+
+
     }
 }
