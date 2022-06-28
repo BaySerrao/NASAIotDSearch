07873e948f166b924bdf74ba22eab29a145692bd
Bay Serrao <serr0063@algonquinlive.ca>
2022-06-28 18:00:56 -0400

    Lab 2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -25,7 +26,7 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
     private ListView lv;
     String name, height, mass;
-    ArrayList<HashMap<String,String>> listo;
+    ArrayList<String> listo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... strings) {
             StringBuilder builder = new StringBuilder();
+
             try {
                 URL url = new URL(api_url);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -69,29 +71,25 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String s) {
-            ArrayList<HashMap<String,String>> listo = new ArrayList<>();
+
             try {
-                JSONObject jobect = new JSONObject();
+                JSONObject jobect = new JSONObject(s);
                 JSONArray jarray = jobect.getJSONArray("results");
                 for (int i = 0; i < jarray.length(); i++) {
                     JSONObject j1 = jarray.getJSONObject(i);
                     name = j1.getString("name");
                     height = j1.getString("height");
                     mass = j1.getString("mass");
-                    HashMap<String,String> n1 = new HashMap<>();
-                    n1.put("name", name);
-                    listo.add(i, n1);
+                    listo.add(name);
 
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            ListAdapter adapter = new SimpleAdapter(
+            ListAdapter adapter = new ArrayAdapter<String>(
                     MainActivity.this,
-                    listo,
                     R.layout.activity_main,
-                    new String[]{"name"},
-                    new int[]{R.id.lv1});
+                    listo);
             lv.setAdapter(adapter);
             super.onPostExecute("done");
         }
