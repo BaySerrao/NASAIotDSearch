e744f0e43a85ec7900e175b033b6ddc0786afeac
Bay Serrao <serr0063@algonquinlive.ca>
2022-07-04 22:22:40 -0400

    Lab 2

---
+++ app/src/main/java/com/example/androidlabs/DadJoke.java
@@ -0,0 +1,83 @@
+package com.example.androidlabs;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class DadJoke extends AppCompatActivity {
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_dad_joke);
+            Toolbar tool = (Toolbar)findViewById(R.id.tool);
+            setSupportActionBar(tool);
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.d1);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, tool,R.string.open, R.string.close);
+            drawer.addDrawerListener(toggle);
+            NavigationView nav = (NavigationView) findViewById(R.id.nav);
+            nav.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+        }
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu items for use in the action bar
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.mainmenny, menu);
+            return true;
+        }
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+
+// Handle presses on the action bar items
+            switch (item.getItemId()) {
+                case R.id.item1:
+                    Toast.makeText(getApplicationContext(),R.string.toast1,Toast.LENGTH_SHORT).show();
+
+                    break;
+                case R.id.item2:
+                    Toast.makeText(getApplicationContext(),R.string.toast2,Toast.LENGTH_SHORT).show();
+                    break;
+
+            }
+            return false;
+        }
+        boolean onNavigationItemSelected(MenuItem item){
+            Toolbar tool = (Toolbar)findViewById(R.id.tool);
+            setSupportActionBar(tool);
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.d1);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, tool,R.string.open, R.string.close);
+            drawer.addDrawerListener(toggle);
+            NavigationView nav = (NavigationView) findViewById(R.id.nav);
+            nav.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+            int id = item.getItemId();
+            if (id == R.id.nav1) {
+                //launch main activity on home button
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+
+            }else if (id==R.id.nav2){
+                //launch dad joke activity
+                Intent intent = new Intent(this, DadJoke.class);
+                startActivity(intent);
+
+            }else if (id==R.id.nav3){
+                //call finishAffinity(); to close and exit
+                finishAffinity();
+                System.exit(0);
+            }
+
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+    }
--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,112 +1,82 @@
 package com.example.androidlabs;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.graphics.Bitmap;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.channels.AsynchronousChannelGroup;
-import java.util.ArrayList;
-import java.util.HashMap;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private ListView lv;
-    String name, height, mass;
-    ArrayList<String> listo;
-    ArrayList<String> listo2;
-    ArrayList<String> listo3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        yoink y1 = new yoink();
-        y1.execute();
-        lv = findViewById(R.id.lv1);
-        EmptyActivity e1 = new EmptyActivity();
-        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        Toolbar tool = (Toolbar)findViewById(R.id.tool);
+        setSupportActionBar(tool);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.d1);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, tool,R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        NavigationView nav = (NavigationView) findViewById(R.id.nav);
+        nav.setNavigationItemSelectedListener(this::onNavigationItemSelected);
 
-            public boolean onItemLongClick(AdapterView<?> arg0, View v,
-                                           int index, long arg3) {
-                e1.doThing();
-                return true;
-            }
-        });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenny, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+// Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(getApplicationContext(),R.string.toast1,Toast.LENGTH_SHORT).show();
 
-    class yoink extends AsyncTask<String, Integer, String> {
-        String api_url = "https://swapi.dev/api/people/?format=json";
-        String name, height, mass;
+                break;
+            case R.id.item2:
+                Toast.makeText(getApplicationContext(),R.string.toast2,Toast.LENGTH_SHORT).show();
+                break;
 
+        }
+        return false;
+    }
+    boolean onNavigationItemSelected(MenuItem item){
+        Toolbar tool = (Toolbar)findViewById(R.id.tool);
+        setSupportActionBar(tool);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.d1);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, tool,R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        NavigationView nav = (NavigationView) findViewById(R.id.nav);
+        nav.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+        int id = item.getItemId();
+        if (id == R.id.nav1) {
+        //launch main activity on home button
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        }else if (id==R.id.nav2){
+            //launch dad joke activity
+            Intent intent = new Intent(this, DadJoke.class);
+            startActivity(intent);
+        }else if (id==R.id.nav3){
+            //call finishAffinity(); to close and exit
 
-        @Override
-        protected String doInBackground(String... strings) {
-            StringBuilder builder = new StringBuilder();
-
-            try {
-                URL url = new URL(api_url);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-                connection.connect();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-                while (true) {
-                    String readLine = reader.readLine();
-                    String data = readLine;
-                    if (data == null) {
-                        break;
-                    }
-                    builder.append(data);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return builder.toString();
+            System.exit(0);
         }
 
-        @Override
-        protected void onPostExecute(String s) {
-            listo = new ArrayList<String>();
-            listo2 = new ArrayList<String>();
-            listo3 = new ArrayList<String>();
-            try {
-                JSONObject jobect = new JSONObject(s);
-                JSONArray jarray = jobect.getJSONArray("results");
-                for (int i = 0; i < jarray.length(); i++) {
-                    JSONObject j1 = jarray.getJSONObject(i);
-                    name = j1.getString("name");
-                    height = j1.getString("height");
-                    mass = j1.getString("mass");
-                    listo.add(name);
-                    listo2.add(height);
-                    listo3.add(mass);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            ListAdapter adapter = new ArrayAdapter<String>(
-                    MainActivity.this,
-                    R.layout.list_filler,
-                    listo);
-            lv.setAdapter(adapter);
-            super.onPostExecute("done");
-        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
-
